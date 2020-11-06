1dcd206cd2437154c9a0d19020cd91ddb2f136cc
Gennaro Bosone <gennaro.bs@gmail.com>
2020-11-06 18:39:26 +0100

    update examples

--- docs-src/js/examples/bind-examples.js
+++ docs-src/js/examples/bind-examples.js
@@ -1,32 +1,49 @@
-const examples = document.getElementById('examples')
+import { ucfirst } from '../utilities'
+
+function compress(data) {
+	return LZString.compressToBase64(JSON.stringify(data))
+		.replace(/\+/g, `-`) // Convert '+' to '-'
+		.replace(/\//g, `_`) // Convert '/' to '_'
+		.replace(/=+$/, ``) // Remove ending '='
+}
 
 async function routeExample(path) {
 	const response = await fetch(`https://api.github.com/repos/genbs/urpflanze/contents/${path}`)
 	const { name, html_url, download_url, content } = await response.json()
 
-	const codesandbox_url = `https://codesandbox.io/s/genbsurpflanze-examples-hfpcb?file=${path.substr(9)}`
+	const rawSource = atob(content)
+	const source = rawSource.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')
 
-	const source = atob(content)
-		.replace(/&/g, '&amp;')
-		.replace(/</g, '&lt;')
-		.replace(/>/g, '&gt;')
-		.replace(/"/g, '&quot;')
+	const parameters = compress({
+		files: {
+			'index.html': {
+				content: rawSource,
+			},
+		},
+	})
+
+	const codesandbox_url = `https://codesandbox.io/api/v1/sandboxes/define?parameters=${parameters}`
+
+	const category = ucfirst(path.split('/')[1])
+	const parsedName = ucfirst(name.slice(3, -5).replace(/-/gi, ' '))
 
 	return `
-		<h1>Example ${name}</h1>
+		<h1>Example: ${category} | ${parsedName}</h1>
 		<h2>Output</h2>
-		<iframe class="example__iframe" src="data:text/html;base64,${content}"></iframe>
-		
+		<div class="example__iframe-container">
+			<iframe class="example__iframe" src="data:text/html;base64,${content}"></iframe>
+		</div>
+
 		<h2>Links</h2>
 		<div class="example__links">
-			<a href="${codesandbox_url}">CodeSandbox</a>
-			<a href="${html_url}">Github</a>
-			<a href="${download_url}">Download</a>
+			Edit on <a target="_blank" href="${codesandbox_url}">CodeSandbox</a>,
+			view source on <a target="_blank" href="${html_url}">Github</a>
+			or <a target="_blank" href="${download_url}">Download</a>
 		</div>
 
 		<h2>Source</h2>
 		<div class="example__code">
-		<pre class="prettyprint"><code translate="no" class="language-html">${source}</code></pre>
+			<pre class="prettyprint"><code translate="no" class="language-html">${source}</code></pre>
 		</div>
     `
 }
--- docs-src/js/examples/get-navigation.js
+++ docs-src/js/examples/get-navigation.js
@@ -13,7 +13,7 @@ async function getNavigation() {
 	const items = data.filter(e => e.type === 'dir')
 
 	for (let i = 0, len = items.length; i < len; i++) {
-		const catName = ucfirst(items[i].name)
+		const catName = ucfirst(items[i].name.slice(3))
 		list[catName] = {}
 		const current = list[catName]
 		const response = await fetch(`https://api.github.com/repos/genbs/urpflanze/contents/${items[i].path}`)
@@ -21,7 +21,7 @@ async function getNavigation() {
 		const childs = data.filter(e => e.type === 'file')
 
 		childs.forEach(({ name, path }) => {
-			current[ucfirst(name.slice(4, -5))] = path
+			current[ucfirst(name.slice(3, -5))] = path
 		})
 	}
 
