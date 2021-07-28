818977834bb7b46e5037d5c4275c75f674a90066
genbs <gennaro.bs@gmail.com>
2021-07-28 10:50:40 +0200

    build: build to CJS/ESM

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,59 @@ const path = require('path')
 const package = require('./package.json')
 const version = JSON.stringify(package.version)
 
-module.exports = (env, argv) => {
-	const bProduction = argv.mode && argv.mode === 'production'
+const plugins = bProduction => [
+	new webpack.DefinePlugin({
+		'process.env.NODE_ENV': JSON.stringify(bProduction ? 'production' : 'development'),
+	}),
+	new webpack.BannerPlugin({
+		banner:
+			`@license UrpflanzeJS v${version}` +
+			`\n[file]` +
+			`\n\nGithub: https://github.com/urpflanze-org/urpflanze` +
+			`\n\nThis source code is licensed under the MIT license found in the\nLICENSE file in the root directory of this source tree.`,
+	}),
+]
 
-	return {
-		entry: {
-			urpflanze: './dist/index.js',
+const umd = (bProduction, bLight) => ({
+	entry: bLight ? './dist/cjs/index-light.js' : './dist/cjs/index.js',
+	output: {
+		filename: 'urpflanze' + (bLight ? '-light' : '') + (bProduction ? '.min' : '') + '.js',
+		path: path.resolve('./build/umd'),
+		library: {
+			name: 'Urpflanze',
+			type: 'umd',
 		},
-		output: {
-			filename: bProduction ? '[name].min.js' : '[name].js',
-			path: path.resolve('./build'),
-			library: 'Urpflanze',
-			libraryTarget: 'umd',
-			globalObject: 'window',
+		globalObject: 'window',
+	},
+	plugins: plugins(bProduction),
+	devtool: bProduction ? undefined : 'source-map',
+	mode: bProduction ? 'production' : 'none',
+})
+
+const esm = (bProduction, bLight) => ({
+	entry: bLight ? './dist/esm/index-light.js' : './dist/esm/index.js',
+	output: {
+		filename: 'urpflanze' + (bLight ? '-light' : '') + (bProduction ? '.min' : '') + '.js',
+		path: path.resolve('./build/esm'),
+		library: {
+			type: 'module',
 		},
-		plugins: [
-			new webpack.DefinePlugin({
-				VERSION: version,
-			}),
-		],
-		resolve: {
-			fallback: {
-				stream: false,
-			},
-		},
-		devtool: argv.mode == 'production' ? undefined : 'source-map',
-		mode: argv.mode,
-		watch: argv.watch,
-	}
-}
+	},
+	plugins: plugins(bProduction),
+	devtool: bProduction ? undefined : 'source-map',
+	mode: bProduction ? 'production' : 'none',
+	experiments: {
+		outputModule: true,
+	},
+})
+
+module.exports = [
+	umd(false, false), // urpflanze.js
+	umd(true, false), // urpflanze.min.js
+	umd(false, true), // urpflanze-light.js
+	umd(true, true), // urpflanze-light.minjs
+	esm(false, false),
+	esm(true, false),
+	esm(false, true),
+	esm(true, true),
+]
